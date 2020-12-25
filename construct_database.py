681d942ca90cef4cab9baaeb0baca004691c0f6d
Zackhardtoname <zack.hardtoname@gmail.com>
2020-12-25 07:42:44 -0500

    Add new members into the database

--- construct_database.py
+++ construct_database.py
@@ -8,12 +8,13 @@ database_name = os.getenv("database")
 
 try:
     with con.cursor() as cur:
+        cur.execute(f'DROP DATABASE IF EXISTS {database_name}')
         cur.execute(f'CREATE DATABASE IF NOT EXISTS {database_name}')
         cur.execute(f'use {database_name}')
         create_user = """
             CREATE TABLE IF NOT EXISTS User(
-               id int NOT NULL PRIMARY KEY,
-               discord_user_id int NOT NULL UNIQUE
+               id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+               discord_user_id VARCHAR(32) NOT NULL UNIQUE
             ); 
         """
         cur.execute(create_user)
@@ -21,10 +22,11 @@ try:
         create_action = """
             CREATE TABLE IF NOT EXISTS Action(
                id int NOT NULL PRIMARY KEY,
-               User_id int NOT NULL,
+               User_id INT NOT NULL,
                category VARCHAR(32) NOT NULL,
                detail VARCHAR(32),
-               creation_time DATETIME
+               creation_time DATETIME,
+               FOREIGN KEY(User_id) REFERENCES User(id)
             ); 
         """
         cur.execute(create_action)
--- dbusage.py
+++ dbusage.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import discord
+from discord import Intents
 import logging
 import dbmanagement as dbm
 from dotenv import load_dotenv
@@ -13,7 +14,7 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="!", intents=Intents.all())
 
 
 @client.event
@@ -23,13 +24,13 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-@client.command()
-async def test(ctx):
-    # Then whenever you need to use it
-    # response = await client.sql.query("SELECT * FROM studies WHERE user_id = %s", user_id)
-    # My class manages arguments
-    # await ctx.send(str(response))
-    print("test")
+# @client.command()
+# async def test(ctx):
+#     # Then whenever you need to use it
+#     # response = await client.sql.query("SELECT * FROM studies WHERE user_id = %s", user_id)
+#     # My class manages arguments
+#     # await ctx.send(str(response))
+#     print("test")
 
 
 @client.event
@@ -40,6 +41,23 @@ async def on_voice_state_update(member, before, after):
     print(member, before, after)
 
 
+@client.event
+async def on_member_join(member):
+    guild = member.guild
+    if guild.system_channel is not None:
+        to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
+        await guild.system_channel.send(to_send)
+    print(member)
+    insert_new_member = f"""
+        INSERT INTO User (discord_user_id)
+        VALUES ({member.id});
+    """
+
+    response = await client.sql.query(insert_new_member)
+    if response:
+        print(response)
+
 client.pool = None
 client.sql = dbm.MySQL(client)
 client.run(os.getenv('bot_token'))
+
