ba705faf5447dcda7202aace35fd4215fa4f178e
Zackhardtoname <zack.hardtoname@gmail.com>
2020-12-26 18:23:22 -0500

    Use ORM to insert data in one transaction

---
+++ insert_data.py
@@ -0,0 +1,18 @@
+import os
+import pymysql
+from dotenv import load_dotenv
+import utilities
+import models
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv("dev.env")
+database_name = os.getenv("database")
+
+engine = utilities.get_engine()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+users = [models.User(discord_user_id=utilities.generate_discord_user_id()) for i in range(1, 11)]
+session.add_all(users)
+
+session.commit()
