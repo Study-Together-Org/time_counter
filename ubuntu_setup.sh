f54f3c2b0c9aaf717cba6a31adc63c43e937571f
Zackhardtoname <zack.hardtoname@gmail.com>
2021-01-11 01:21:25 -0500

    Write detailed Ubuntu setup guide

---
+++ ubuntu_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo apt update
+
+sudo apt-get install python3.8-venv python3.8-dev
+python3.8 -m venv venv
+source venv/bin/activate
+
+sudo apt install mariadb-server
+sudo mysql_secure_installation  # manual choices needed
+# https://stackoverflow.com/questions/39281594/error-1698-28000-access-denied-for-user-rootlocalhost/42742610#42742610
+sudo systemctl enable --now mysql
+# to test: mysql -u root -p
+
+sudo apt install redis-server
+sudo systemctl enable redis-server
+# to test: redis-cli
+
+# manual: create a database called studytogether
+# manual: set up dev.env config (create bot etc)
+
+python models.py
+python create_roles.py
+python get_monitored_categories.py
+python insert_real_data.py  # optional
+touch discord.log
+touch heartbeat.log
+
+python main.py 2>&1 > main.log
+tail -F discord.log
+# wait for login log to show
