46fa3f02d2fc04f79e4f0c22c52a8aa6c085db42
Zackhardtoname <zack.hardtoname@gmail.com>
2021-02-01 10:06:18 -0500

    Add test cases

--- tests_common.py
+++ tests_common.py
@@ -25,3 +25,23 @@ def test_get_day_start_today_slightly_after():
 @freeze_time("2020-01-01 21:12:34")
 def test_get_day_start_today_after():
     assert str(utilities.get_day_start()) == "2020-01-01 17:00:00"
+
+
+@freeze_time("2020-01-31 21:12:34")
+def test_get_month_start_last_day():
+    assert str(utilities.get_month_start()) == "2020-01-01 17:00:00"
+
+
+@freeze_time("2020-02-01 15:12:34")
+def test_get_month_start_day_one():
+    assert str(utilities.get_month_start()) == "2020-01-01 17:00:00"
+
+
+@freeze_time("2020-02-01 15:12:34")
+def test_get_month_start_day_one_near():
+    assert str(utilities.get_month_start()) == "2020-01-01 17:00:00"
+
+
+@freeze_time("2020-02-01 17:12:34")
+def test_get_month_start_day_one_after():
+    assert str(utilities.get_month_start()) == "2020-02-01 17:00:00"
