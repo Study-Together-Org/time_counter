ac56d24e234b36f5d748d38a11c42f7ec7f121dd
Cole Killian <g.cole.killian@gmail.com>
2021-05-11 22:55:15 -0400

    Update redis_to_sql.py

--- redis_to_sql.py
+++ redis_to_sql.py
@@ -1,55 +1,48 @@
 #!/usr/bin/env python
-from datetime import datetime
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-import os, sys
-import redis
-from models import Action, User, DailyHours
 import argparse
+import os
+from datetime import datetime
 
-mode="development"
+import redis
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import DailyHours
+import utilities
+
+load_dotenv("dev.env")
+
+mode = "development"
 
 # command line arguments
 parser = argparse.ArgumentParser(description="Migrate redis sets to mysql")
 parser.add_argument('time', help="The time of the redis set to migrate in format hh:mm:ss")
-parser.add_argument('--date', default=datetime.today().strftime('%Y-%m-%d'), help="The date of the redis set to migrate in format %Y-%m-%d. Default is today's date")
+parser.add_argument('--date', default=datetime.today().strftime('%Y-%m-%d'),
+                    help="The date of the redis set to migrate in format Y-m-d. Default is today's date")
 args = parser.parse_args()
 
 sorted_set_name = f"daily_{args.date} {args.time}"
 sorted_set_datetime = datetime.strptime(sorted_set_name, 'daily_%Y-%m-%d %H:%M:%S')
-print(sorted_set_name, sorted_set_datetime)
 
 echo = True
 
-engine = create_engine(
-    f'mysql+pymysql://{os.getenv("sql_user")}:{os.getenv("sql_password")}@{os.getenv("sql_host")}/{os.getenv("sql_database")}',
-    echo=echo)
+# get engine
+engine = utilities.get_engine()
+# get redis client
+redis_client = utilities.get_redis_client()
 
-redis_client = redis.Redis(
-    host=os.getenv("redis_host"),
-    port=os.getenv("redis_port"),
-    db=int(os.getenv("redis_db_num")),
-    username=os.getenv("redis_username"),
-    password=os.getenv("redis_password"),
-    decode_responses=True
-)
-
-# insert from redis set into mysql
+# get session object from engine
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# add to session
 for rank, row in enumerate(redis_client.zrangebyscore(sorted_set_name, "-inf", "inf", withscores=True)):
-    # get time from in_session
-    # do we need this?
-    in_session_time = redis_client.hget(f"in_session_{sorted_set_name}", row[0])
-    in_session_time = float(in_session_time) if in_session_time else 0
-
     if mode == "production":
         session.add(DailyHours(user_id=row[0], timestamp=sorted_set_datetime, study_time=round(row[1], 3), rank=rank))
     else:
         print(f"user_id={row[0]}, timestamp={sorted_set_datetime}, study_time={round(row[1], 3)}, rank={rank}")
 
-# uncomment for production
 if mode == "production":
     session.commit()
     redis_client.delete(sorted_set_name)
