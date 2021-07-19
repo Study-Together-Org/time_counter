52a2f86c3b930b19237c5b8537dcd226c2d75d34
Zack Light <zack.hardtoname@gmail.com>
2021-07-19 10:38:40 -0700

    Simplify restart.sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-source .venv/bin/activate
-nohup python controller_time_counter.py > console.log 2>&1 &
+python timezone_bot.py &
+python controller_time_counter.py > console.log 2>&1
